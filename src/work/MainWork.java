b6f6ca4723ad646170033402b3dbc8accda85142
EgorOsipov <osipovea@mai.ru>
2025-06-23 16:50:21 +0300

    Add work with files

--- src/work/MainWork.java
+++ src/work/MainWork.java
@@ -1,191 +1,146 @@
 package work;
+
+import java.io.*;
 import java.util.Scanner;
-import java.util.Random;
 
 public class MainWork {
-	public static void main(String[] args)
-	{
-		int choice;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1. Создание массива:");
-		WorkWithArray arrayEx = new WorkWithArray();
-		int[] generatedArray = arrayEx.generateArray();
-		
-		System.out.println("Созданный массив: ");
-		WorkWithArray.printArray(generatedArray);
-		
-		System.out.println();
-		System.out.println("2. Выберите метод поиска ключа по значению (если ключ -1 - значение не найдено): ");
-		System.out.println("1) линейный поиск;");
-		System.out.println("2) бинарный поиск.");
-		choice = scanner.nextInt();
-		
-		switch(choice)
-		{
-		case 1:
-		System.out.println("Ключ (номер в массиве) выбранного вами значения - " + arrayEx.linearSearch(generatedArray));
-		break;
-		
-		case 2:
-			System.out.println("Ключ (номер в массиве) выбранного вами значения - "  + arrayEx.binarySearch(generatedArray));
-			break;
-		default:
-			System.out.println("Ошибка ввода.");
-		}
-		
-		
-			
-		
-		
-		
-	}
-}
+    public static void main(String[] args) {
+        // 1. Настройка файлов
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+        
+        // 2. Настройка ввода/вывода
+        Scanner consoleScanner = new Scanner(System.in);
+        PrintWriter writer = null;
+        
+        try {
+            // 3. Проверка входного файла
+            System.out.println("Пытаюсь прочитать файл: " + new File(inputFile).getAbsolutePath());
+            
+            if (!new File(inputFile).exists()) {
+                System.out.println("ОШИБКА: Файл input.txt не найден!");
+                System.out.println("Поместите файл в: " + new File("").getAbsolutePath());
+                return;
+            }
 
-class WorkWithArray
-{
-	static void printArray(int[] array)
-	{
-		for(int i = 0; i < array.length; i++)
-		{
-			System.out.print(array[i] + " ");
-		}
-	}
-	
-	int[] manualInput(int[] array)
-	{
-		Scanner scanner = new Scanner(System.in);
-		
-		for(int i = 0; i < array.length; i++)
-		{
-			array[i] = scanner.nextInt();
-		}
-		return array;
-	}
-	
-	int[] randomInput(int[] array)
-	{
-		Random rand = new Random();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Введите нижнюю границу для рандомной генерации: ");
-		int min = scanner.nextInt();
-		System.out.println("Введите верхнюю границу для рандомной генерации: ");
-		int max = scanner.nextInt();
-		
-		for(int i = 0; i < array.length; i++)
-		{
-			array[i] = rand.nextInt(min, max);
-		}
-		
-		return array;
-	}
-	
-	int[] choiceSort(int[] array)
-	{
-		for(int i = 0; i < array.length - 1; i++)
-		{
-			int minIndex = i;
-			
-			for(int j = i + 1; j < array.length; j++)
-			{
-				if(array[j] < array[minIndex]) minIndex = j;
-			}
-			
-			if(minIndex != i)
-			{
-				int temp = array[i];
-				array[i] = array[minIndex];
-				array[minIndex] = temp;
-			}
-		}
-		
-		System.out.println("Отсортированный массив: ");
-		printArray(array);
-		return array;
-	}
-	
-	public int[] generateArray()
-	{
-		int size;
-		Random rand = new Random();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Введите размер массива: ");
-		size = scanner.nextInt();
-		
-		int[] array = new int[size];
-		
-		System.out.println("Выберити способ заполнения массива: (1 - вручную; 2 - рандомными числами):");
-		int choice = scanner.nextInt();
-		
-		switch(choice)
-		{
-		case 1:
-			manualInput(array);
-			break;
-			
-		case 2:
-			randomInput(array);
-			break;
-			
-		default:
-			System.out.println("Вы ввели некорректное число, попробуйте заново");
-			break;
-		}
-		
-		return array;
-	}
-	
-	 int linearSearch(int[] array)
-	{
-		Scanner scanner = new Scanner(System.in);
-		int[] sortArray = choiceSort(array);
-		System.out.println();
-		System.out.println("Выберите значение элемента, который хотите найти: ");
-		int value = scanner.nextInt();
-		int key;
-		
-		for(int i = 0; i < array.length; i++)
-		{
-			if(array[i] == value) 
-				{
-				key = i;
-				return key;
-				}
-			
-		}
-		
-		return -1;
-	}
-	
-	int binarySearch(int[] array)
-	{
-		Scanner scanner = new Scanner(System.in);
-		int[] sortArray = choiceSort(array);
-		System.out.println();
-		System.out.println("Выберите значение элемента, который хотите найти: ");
-		int value = scanner.nextInt();
-		int key;
-		
-		int low = 0;
-		int high = array.length - 1;
-		
-		while(low <= high)
-		{
-			int mid = (low + high) / 2;
-			
-			if(array[mid] == value)
-			{
-				return mid;
-			}
-			else if(array[mid] < value)
-			{
-				low = mid + 1;
-			}
-			else if(array[mid] > value)
-			{
-				high = mid - 1;
-			}
-		}
-		return -1;
-	}
+            // 4. Чтение массива
+            System.out.println("\n=== Чтение массива из файла ===");
+            int[] array = readArrayFromFile(inputFile);
+            System.out.println("Прочитано " + array.length + " элементов");
+
+            // 5. Настройка вывода в файл
+            writer = new PrintWriter(new FileWriter(outputFile));
+            System.out.println("\n=== Выбор метода поиска ===");
+            
+            // 6. Выбор метода
+            System.out.println("Доступные методы:");
+            System.out.println("1. Линейный поиск");
+            System.out.println("2. Бинарный поиск (массив будет отсортирован)");
+            System.out.print("Ваш выбор (1/2): ");
+            
+            int choice = consoleScanner.nextInt();
+            writer.println("Выбран метод: " + (choice == 1 ? "линейный" : "бинарный"));
+
+            // 7. Ввод значения для поиска
+            System.out.print("\nВведите значение для поиска: ");
+            int value = consoleScanner.nextInt();
+            writer.println("Искомое значение: " + value);
+
+            // 8. Выполнение поиска
+            System.out.println("\n=== Результаты ===");
+            int result = -1;
+            
+            if (choice == 1) {
+                result = linearSearch(array, value);
+            } else if (choice == 2) {
+                array = sortArray(array);
+                System.out.println("Массив отсортирован");
+                writer.println("Отсортированный массив:");
+                printArray(array, writer);
+                result = binarySearch(array, value);
+            }
+
+            // 9. Вывод результатов
+            String resultStr = "Результат: " + (result != -1 ? "найден на позиции " + result : "не найден");
+            System.out.println(resultStr);
+            writer.println(resultStr);
+
+        } catch (IOException e) {
+            System.out.println("ОШИБКА: " + e.getMessage());
+        } finally {
+            if (writer != null) writer.close();
+            consoleScanner.close();
+            System.out.println("\nПрограмма завершена. Результаты в " + outputFile);
+        }
+    }
+
+    private static int[] readArrayFromFile(String filename) throws IOException {
+        Scanner fileScanner = new Scanner(new File(filename));
+        int size = fileScanner.nextInt();
+        int[] array = new int[size];
+        
+        System.out.println("Содержимое файла:");
+        for (int i = 0; i < size; i++) {
+            array[i] = fileScanner.nextInt();
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        
+        fileScanner.close();
+        return array;
+    }
+
+    private static void printArray(int[] array, PrintWriter writer) {
+        for (int num : array) {
+            writer.print(num + " ");
+        }
+        writer.println();
+    }
+    
+    // Сортировка массива
+    private static int[] sortArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+        return array;
+    }
+
+    // Линейный поиск
+    private static int linearSearch(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Бинарный поиск
+    private static int binarySearch(int[] array, int value) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (array[mid] == value) {
+                return mid;
+            } else if (array[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
